b462779e36708753120c465e000c69117b761858
mmttyrm <mmttyrm@gmail.com>
2023-11-24 15:27:50 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,39 @@
 // import Image from 'next/image';
 import styles from './page.module.css';
-
+import classNames from 'classnames/bind';
+import mustang from '@/assets/images/mustang1.png';
+const cx = classNames.bind(styles);
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <h1>Mustang</h1>
+        <main className={cx('container', 'wrapper')}>
+            <div className='row'>
+                <div className='col-5'>
+                    <div className='content__header'>
+                        <h1>Ford Mustang Shelby GT350R</h1>
+                        <p>
+                            As a track-ready version of the Ford Mustang, the Shelby GT350 has even
+                            more muscular styling and next-level performace
+                        </p>
+                    </div>
+                    <div className={cx('price')}>
+                        <span>Starting At</span>
+                        <p>68,230</p>
+                    </div>
+                </div>
+                <div className='col-7'>
+                    <div className='image__content'>
+                        <img className={cx('image__header')} src={mustang.src} alt='mustang' />
+                    </div>
+                    <div className={cx('info', 'grid')}>
+                        <div className='row'>
+                            <div className='col'>1</div>
+                            <div className='col'>1</div>
+                            <div className='col'>1</div>
+                            <div className='col'>1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,22 +2,44 @@
 import Link from 'next/link';
 import classNames from 'classnames/bind';
 import style from './header.module.css';
+import Logo from '@/assets/svg/logo';
 
 const cx = classNames.bind(style);
 function Header() {
     return (
-        <div className={cx('header')}>
-            <h1>logo</h1>
-            <ul>
-                <li>
-                    {''}
-                    <Link href={'#'}>abc</Link>
-                </li>
-                <li> btn2</li>
-                <li> btn3</li>
-                <li> btn4</li>
-                <li> btn5</li>
-            </ul>
+        <div className={cx('wrapper', 'container')}>
+            <Link href={'#'} className={cx('logo')}>
+                <Logo fillColor={'#fafafa'} width={100} />
+                <h1 className={cx('logo-text')}>FORDEST</h1>
+            </Link>
+
+            <div className={cx('navbar')}>
+                <Link href={'#'} className={cx('nav-item', 'active')}>
+                    Models
+                </Link>
+                <Link href={'#'} className={cx('nav-item')}>
+                    Technology
+                </Link>
+                <Link href={'#'} className={cx('nav-item')}>
+                    History
+                </Link>
+                <Link href={'#'} className={cx('nav-item')}>
+                    Shop
+                </Link>
+                <Link href={'#'} className={cx('nav-item')}>
+                    Owner
+                </Link>
+            </div>
+            <div className={cx('control')}>
+                <div className={cx('user')}>
+                    <img
+                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlK-Ky4QaogWNWEZccL7rhvhQXP3tksjjLgX3v3jC2Tg&s'
+                        alt=''
+                        className={cx('avatar')}
+                    />
+                </div>
+                <div className={cx('language')}>English</div>
+            </div>
         </div>
     );
 }
